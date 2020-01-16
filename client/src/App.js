dd6a002053b0edb935ec03a638e76038aaa6cfcf
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-16 15:44:22 +0000

    end point working

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [message, setMessage] =useState('')
+
+  useEffect(() => {
+    fetch(`/api/message?name=colin`)
+      .then(res => res.json())
+      .then(message => setMessage(message))
+      .catch(error => {
+        debugger
+      })
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { message }
       </header>
     </div>
   );
